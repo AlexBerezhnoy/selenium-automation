b0e2c9ae22a2bbe6f9aa40e5edabe1b1e35374e8
AlexBerezhnoy <dinnylol221@gmail.com>
2020-05-15 02:50:39 +0300

    some fix HW #18

---
+++ src/test/java/io/react/realworld/NewPostTest.java
@@ -0,0 +1,104 @@
+package io.react.realworld;
+
+import com.hillel.auto.model.User;
+import com.hillel.auto.page.object.*;
+import com.hillel.auto.utils.UserData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NewPostTest extends TestBase {
+    private User user = UserData.defaultUser();
+    String title = "New Test Post";
+    String about = "For testing";
+    String postText = "Post For testing";
+    String tags = "Tags!";
+
+    @Test(priority = 1 )
+    public void createPost () {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EE MMM dd yyyy").withLocale(Locale.ENGLISH);;
+        String nowDate = date.format(formatter);
+        clickLoginButton();
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = loginPage.login(user.getEmail(), user.getPassword());
+        NewPostPage newPostPage = homePage.clickNewPost();
+        newPostPage.inputArticleTitle(title);
+        newPostPage.inputWhatArticleAbout(about);
+        newPostPage.inputArticle(postText);
+        newPostPage.inputTags(tags);
+        ArticleDetailsPage articleDetailsPage = newPostPage.clickPublishArticleBtn();
+        assertThat(articleDetailsPage.getPostText()).isEqualTo(postText);
+        String datePost = getDatePost().getText();
+        assertThat(datePost).isEqualTo(nowDate);
+
+    }
+
+    @Test(priority = 2 )
+    public void editPost () {
+        clickLoginButton();
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = loginPage.login(user.getEmail(), user.getPassword());
+        ProfilePage profilePage = homePage.clickProfile();
+        profilePage.clickMyArticles();
+        ArticleDetailsPage articleDetailsPage = profilePage.clickLastPost();
+        NewPostPage newPostPage = articleDetailsPage.clickEditButton();
+
+        postText = postText+" EDIT";
+        newPostPage.inputArticleTitle(title);
+        newPostPage.inputWhatArticleAbout(about);
+        newPostPage.inputArticle(postText);
+        newPostPage.inputTags(tags);
+        articleDetailsPage = newPostPage.clickPublishArticleBtn();
+        assertThat(articleDetailsPage.getPostText()).isEqualTo(postText);
+
+
+    }
+
+    @Test (priority = 3 )
+    public void deletePost () {
+        clickLoginButton();
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = loginPage.login(user.getEmail(), user.getPassword());
+        ProfilePage profilePage = homePage.clickProfile();
+        profilePage.clickMyArticles();
+        ArticleDetailsPage articleDetailsPage = profilePage.clickLastPost();
+        homePage = articleDetailsPage.clickDeleteButton();
+        profilePage = homePage.clickProfile();
+
+        WebElement myArticlesPosts = driver.findElement(By.cssSelector(".article-preview"));
+        assertThat(myArticlesPosts.getText()).isEqualTo("No articles are here... yet.");
+
+    }
+
+
+    public void clickMyArticles() {
+        WebElement myPost = driver.findElement(By.cssSelector("#main > div > div > div.container > div > div > div.articles-toggle > ul > li:nth-child(1) > a"));
+        myPost.click();
+    }
+
+    public void clickLastPost () {
+        WebElement lastPost = driver.findElement(By.cssSelector("#main > div > div > div.container > div > div > div:nth-child(2) > div > a"));
+        lastPost.click();
+    }
+
+    public void clickEditButton () {
+        WebElement editButton = driver.findElement(By.cssSelector("#main > div > div > div.banner > div > div > span > a"));
+        editButton.click();
+    }
+
+    public void clickDeleteButton () {
+        WebElement deleteButton = driver.findElement(By.cssSelector("#main > div > div > div.banner > div > div > span > button"));
+        deleteButton.click();
+    }
+
+    public WebElement getDatePost () {
+        return driver.findElement(By.cssSelector("#main > div > div > div.banner > div > div > div > span"));
+    }
+}
